9620246bf00e365114a184abe39f5c63883c7868
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-09-29 12:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table = document.querySelector("table");
+let prices = document.querySelectorAll(".price");
+
 const getSum = () => {
 //Add your code here
-  
+	let total = 0;
+	prices.forEach((el)=>{
+		total+=Number(el.innerText);
+	});
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  td.innerText = total;
+  td.className = "price";
+  tr.append(td);
+  table.append(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
